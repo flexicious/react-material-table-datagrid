b9dad9ab013b4107ca70823c94f23360209fd38a
SH <flexicious@gmail.com>
2019-02-02 02:20:48 -0500

    Fix for tristate checkbox

--- src/material/grid/MaterialTristateCheckBox.js
+++ src/material/grid/MaterialTristateCheckBox.js
@@ -3,27 +3,27 @@ import { Checkbox } from '@material-ui/core';
 import React from 'react';
 
 export default class MaterialTristateCheckBox extends TriStateCheckBox {
- 
-    render(){
+    render() {
         let cb;
         {
-            cb= this.getMiddle()?this.getEnabled()?
-            <Checkbox indeterminate/>:<Checkbox indeterminate disabled/>:
-            this.getEnabled()?this.getSelected()?
-            <Checkbox checked/>:<Checkbox/>:
-            this.getSelected()?
-            <Checkbox checked disabled/>:<Checkbox disabled/>;
+            cb = this.getMiddle() ? this.getEnabled() ?
+                <Checkbox indeterminate checked={false}/> : <Checkbox checked={false} indeterminate disabled /> :
+                this.getEnabled() ? this.getSelected() ?
+                    <Checkbox checked /> : <Checkbox checked={false}/> :
+                    this.getSelected() ?
+                        <Checkbox checked disabled /> : <Checkbox disabled checked={false}/>;
         }
 
         this.setAttribute("className", "")
-        this.children=[cb];
+        this.children = [cb];
         return super.render();
     }
     attachClass(newClass) {
 
     }
-    setSelectedState(val){
+    setSelectedState(val) {
         super.setSelectedState(val);
+        this.render();
         this.invalidateState();
     }
 }
